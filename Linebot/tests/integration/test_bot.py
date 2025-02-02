79215e066e811d3c2ad86e0bcb06f4c84f113d83
畑迫睦和 <h.yoshikazu0110@gmail.com>
2025-02-02 12:05:30 +0900

    テストの追加

---
+++ Linebot/tests/integration/test_bot.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+load_dotenv()
+from ...services.llama_service import LlamaService
+from ...services.file_service import FileService
+
+import unittest
+
+class Test_TestFindAnswer(unittest.TestCase):
+    def test_find_answer1(self):
+        user_message = "歴史について教えてください。"
+        fileService = FileService()
+        file_path = "LineAutobot\\Data"
+        file_name = "datas\\qaemb.json"
+        embeddings = fileService.read_json_from_file(file_path, file_name)
+
+        llamaService = LlamaService()
+        llamaService.add_embeddings(embeddings)
+        ai_response = llamaService.find_best_answer(user_message)
+        print(ai_response)
+
+if __name__ == '__main__':
+    unittest.main()
